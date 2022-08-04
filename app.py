f8830c30d5b4e21acb356cda077d3aa30a9791f7
Ольга <devyatovka.0993@mail.ru>
2022-08-04 19:05:03 +0300

    добавление запси в БД

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Item(db.Model):
@@ -11,7 +12,6 @@ class Item(db.Model):
     title = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     active = db.Column(db.Boolean, default=True)
-    photo = db.Column (db.LargeBinary, nullable=False)
     text = db.Column(db.Text, nullable=False)
 
 @app.route('/')
@@ -22,9 +22,25 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/create')
+@app.route('/create', methods=['POST','GET'])
 def create():
-    return render_template('create.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+        text = request.form['text']
+
+        item = Item(title=title, price=price, text=text)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Произошла ошибка'
+
+    else:
+        return render_template('create.html')
+
 
 @app.route('/homepage')
 def homepage():
