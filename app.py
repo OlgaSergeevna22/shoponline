2a44a25a627f268c25c6d35c37abf84652bb02a3
Ольга <devyatovka.0993@mail.ru>
2022-08-04 12:45:16 +0300

    add_fields_db

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy
 
-
+class Item(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    active = db.Column(db.Boolean, default=True)
+    photo = db.Column (db.LargeBinary, nullable=False)
+    text = db.Column(db.Text, nullable=False)
 
 @app.route('/')
 def index():
